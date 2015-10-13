f665cd9b0b64e4d5cb142aaf19b922040c7ac23e
Ryan Eagan <eaganry@gmail.com>
2015-10-13 14:49:16 -0400

    Removed a few unneeded dependencies

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 var mysql = require('mysql');
 
 var loadIndex = require('./routes/loadIndex');
@@ -17,10 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, 'public', 'logo.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
